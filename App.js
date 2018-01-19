d198e4d601e27b235cb94fbac471f901d9108adc
PriyaG88 <priya.g71488@gmail.com>
2018-01-18 16:20:14 -0800

    add add'l tab navigation to main app component

--- App.js
+++ App.js
@@ -4,18 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen }
+        })
+      }
     });
 
     return (
-
       <MainNavigator />
-
     );
   }
 }
